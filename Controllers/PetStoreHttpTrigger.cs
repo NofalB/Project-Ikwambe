412d14fe0953b3133d179f00814d70c102a4af3f
NofalB <42965940+622022@users.noreply.github.com>
2021-09-09 11:52:36 +0200

    Code added

---
+++ Controllers/PetStoreHttpTrigger.cs
@@ -0,0 +1,117 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Petstore.Models;
+using Microsoft.OpenApi.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Petstore.Controllers {
+	public class PetStoreHttpTrigger {
+		ILogger Logger { get; }
+
+		public PetStoreHttpTrigger(ILogger<PetStoreHttpTrigger> Logger) {
+			this.Logger = Logger;
+		}
+
+		[Function(nameof(PetStoreHttpTrigger.AddPet))]
+		[OpenApiOperation(operationId: "addPet", tags: new[] { "pet" }, Summary = "Add a new pet to the store", Description = "This add a new pet to the store.", Visibility = OpenApiVisibilityType.Important)]
+		//[OpenApiSecurity("petstore_auth", SecuritySchemeType.Http, In = OpenApiSecurityLocationType.Header, Scheme = OpenApiSecuritySchemeType.Bearer, BearerFormat = "JWT")]
+		[OpenApiRequestBody(contentType: "application/json", bodyType: typeof(User), Required = true, Description = "Pet object that needs to be added to the store")]
+		[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(User), Summary = "New pet details added", Description = "New pet details added", Example = typeof(DummyPetExample))]
+		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.MethodNotAllowed, Summary = "Invalid input", Description = "Invalid input")]
+		public async Task<HttpResponseData> AddPet([HttpTrigger(AuthorizationLevel.Function, "POST", Route = "pet")] HttpRequestData req, FunctionContext executionContext) {
+			// Parse input
+			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+			User pet = JsonConvert.DeserializeObject<User>(requestBody);
+
+			pet.Id += 100;
+
+			// Generate output
+			HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
+
+			await response.WriteAsJsonAsync(pet);
+
+			return response;
+		}
+
+		[Function(nameof(PetStoreHttpTrigger.UpdatePet))]
+		[OpenApiOperation(operationId: "updatePet", tags: new[] { "pet" }, Summary = "Update an existing pet", Description = "This updates an existing pet.", Visibility = OpenApiVisibilityType.Important)]
+		//[OpenApiSecurity("petstore_auth", SecuritySchemeType.Http, In = OpenApiSecurityLocationType.Header, Scheme = OpenApiSecuritySchemeType.Bearer, BearerFormat = "JWT")]
+		[OpenApiRequestBody(contentType: "application/json", bodyType: typeof(User), Required = true, Description = "Pet object that needs to be updated to the store")]
+		[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(User), Summary = "Pet details updated", Description = "Pet details updated", Example = typeof(DummyPetExample))]
+		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid ID supplied", Description = "Invalid ID supplied")]
+		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NotFound, Summary = "Pet not found", Description = "Pet not found")]
+		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.MethodNotAllowed, Summary = "Validation exception", Description = "Validation exception")]
+		public async Task<HttpResponseData> UpdatePet([HttpTrigger(AuthorizationLevel.Function, "PUT", Route = "pet")] HttpRequestData req, FunctionContext executionContext) {
+			// Parse input
+			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+			User pet = JsonConvert.DeserializeObject<User>(requestBody);
+
+			// Generate output
+			HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
+
+			await response.WriteAsJsonAsync(pet);
+
+			return response;
+		}
+
+		[Function(nameof(PetStoreHttpTrigger.FindByStatus))]
+		[OpenApiOperation(operationId: "findPetsByStatus", tags: new[] { "pet" }, Summary = "Finds Pets by status", Description = "Multiple status values can be provided with comma separated strings.", Visibility = OpenApiVisibilityType.Important)]
+		//[OpenApiSecurity("petstore_auth", SecuritySchemeType.Http, In = OpenApiSecurityLocationType.Header, Scheme = OpenApiSecuritySchemeType.Bearer, BearerFormat = "JWT")]
+		[OpenApiParameter(name: "status", In = ParameterLocation.Query, Required = true, Type = typeof(List<PetStatus>), Summary = "Pet status value", Description = "Status values that need to be considered for filter", Visibility = OpenApiVisibilityType.Important)]
+		[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(List<User>), Summary = "successful operation", Description = "successful operation", Example = typeof(DummyPetExamples))]
+		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid status value", Description = "Invalid status value")]
+		public async Task<HttpResponseData> FindByStatus([HttpTrigger(AuthorizationLevel.Function, "GET", Route = "pet/findByStatus")] HttpRequestData req, FunctionContext executionContext) {
+			// Generate output
+			HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
+
+			List<User> pets = new List<User>() { };
+
+			await response.WriteAsJsonAsync(pets);
+
+			return response;
+		}
+
+		[Function(nameof(PetStoreHttpTrigger.FindByTags))]
+		[OpenApiOperation(operationId: "findPetsByTags", tags: new[] { "pet" }, Summary = "Finds Pets by tags", Description = "Muliple tags can be provided with comma separated strings.", Visibility = OpenApiVisibilityType.Important)]
+		//[OpenApiSecurity("petstore_auth", SecuritySchemeType.Http, In = OpenApiSecurityLocationType.Header, Scheme = OpenApiSecuritySchemeType.Bearer, BearerFormat = "JWT")]
+		[OpenApiParameter(name: "tags", In = ParameterLocation.Query, Required = true, Type = typeof(List<string>), Summary = "Tags to filter by", Description = "Tags to filter by", Visibility = OpenApiVisibilityType.Important)]
+		[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(List<User>), Summary = "successful operation", Description = "successful operation", Example = typeof(DummyPetExamples))]
+		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid tag value", Description = "Invalid tag value")]
+		public async Task<HttpResponseData> FindByTags([HttpTrigger(AuthorizationLevel.Function, "GET", Route = "pet/findByTags")] HttpRequestData req, FunctionContext executionContext) {
+			// Generate output
+			HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
+
+			List<User> pets = new List<User>() { };
+
+			await response.WriteAsJsonAsync(pets);
+
+			return response;
+		}
+
+		[Function(nameof(PetStoreHttpTrigger.GetPetById))]
+		[OpenApiOperation(operationId: "getPetById", tags: new[] { "pet" }, Summary = "Find pet by ID", Description = "Returns a single pet.", Visibility = OpenApiVisibilityType.Important)]
+		//[OpenApiSecurity("petstore_auth", SecuritySchemeType.Http, In = OpenApiSecurityLocationType.Header, Scheme = OpenApiSecuritySchemeType.Bearer, BearerFormat = "JWT")]
+		[OpenApiParameter(name: "petId", In = ParameterLocation.Path, Required = true, Type = typeof(long), Summary = "ID of pet to return", Description = "ID of pet to return", Visibility = OpenApiVisibilityType.Important)]
+		[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(User), Summary = "successful operation", Description = "successful operation", Example = typeof(DummyPetExamples))]
+		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid ID supplied", Description = "Invalid ID supplied")]
+		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.NotFound, Summary = "Pet not found", Description = "Pet not found")]
+		public async Task<HttpResponseData> GetPetById([HttpTrigger(AuthorizationLevel.Function, "GET", Route = "pet/{petId}")] HttpRequestData req, long petId, FunctionContext executionContext) {
+			// Generate output
+			HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
+
+			User pet = new User();
+
+			await response.WriteAsJsonAsync(pet);
+
+			return response;
+		}
+	}
+}
---
+++ Models/Category.cs
@@ -0,0 +1,14 @@
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+
+namespace Petstore.Models {
+	/// <summary>
+	/// This represents the model entity for category of Swagger Pet Store.
+	/// </summary>
+	public class Category {
+		[OpenApiProperty(Description = "Gets or sets the category ID.")] 
+		public long? Id { get; set; }
+
+		[OpenApiProperty(Description = "Gets or sets the name.")]
+		public string Name { get; set; }
+	}
+}
