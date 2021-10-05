a21c5e0b5ac859de4f3c046fcbfa7fb401e7e6b0
Nofal <42965940+NofalB@users.noreply.github.com>
2021-10-05 15:05:52 +0200

    modified function name and order request params

--- PaymentMicroservices/PaymentMicroservices/Function1.cs
+++ PaymentMicroservices/PaymentMicroservices/Function1.cs
@@ -1,49 +1,82 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net;
+using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using PaymentMicroservices.Services;
 using PayPalCheckoutSdk.Orders;
 
 namespace PaymentMicroservices
 {
     public static class Function1
     {
-        [Function("Function1")]
-        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req,
+        [Function("CreatePayment")]
+        public static async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req,
             FunctionContext executionContext)
         {
-            var logger = executionContext.GetLogger("Function1");
-            logger.LogInformation("C# HTTP trigger function processed a request.");
+            Console.WriteLine("Create Order with minimum payload..");
+            var request = new OrdersCreateRequest();
+            request.Headers.Add("prefer", "return=representation");
+            //request.Headers.Add("authorization","Basic<AVqSBkkqxw0jOZsOL1meIPnI25Ozcj12Ec8xgZqTrf6R80GftrWaxPDFvC0j4YO2K_Kyze2yNTgwhPRi:EBBGNsdoiHdTKNZk1ax86VSNmzxIYG3A1rO2m0C3A2Hoa24Z5hvrQsm2oZmnF0KF2dL3iOkQNtf7tob5>");
+            request.RequestBody(BuildRequestBodyWithMinimumFields());
+            var response = await PaypalClientService.Client().Execute(request);
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            
+            
+            var result = response.Result<Order>();
+            List<String> paymentLinks = new List<string>();
 
+            Console.WriteLine("Status: {0}", result.Status);
+            Console.WriteLine("Order Id: {0}", result.Id);
+            Console.WriteLine("Intent: {0}", result.CheckoutPaymentIntent);
+            Console.WriteLine("Links:");
+            foreach (LinkDescription link in result.Links)
+            {
+                Console.WriteLine("\t{0}: {1}\tCall Type: {2}", link.Rel, link.Href, link.Method);
+                paymentLinks.Add(link.Rel);
+                paymentLinks.Add(link.Href);
+                paymentLinks.Add(link.Method);
 
-            var order = new OrderRequest()
+            }
+            AmountWithBreakdown amount = result.PurchaseUnits[0].AmountWithBreakdown;
+            Console.WriteLine("Total Amount: {0} {1}", amount.CurrencyCode, amount.Value);
+
+            var response1 = req.CreateResponse(HttpStatusCode.OK);
+
+            //returns header and status code of the response
+            //await response1.WriteAsJsonAsync(response);
+
+            await response1.WriteAsJsonAsync(result.ToString());
+            return response1;
+        }
+
+        private static OrderRequest BuildRequestBodyWithMinimumFields()
+        {
+            OrderRequest orderRequest = new OrderRequest()
             {
                 CheckoutPaymentIntent = "CAPTURE",
-                PurchaseUnits = new List<PurchaseUnitRequest>()
+                ApplicationContext = new ApplicationContext
                 {
-                    new PurchaseUnitRequest()
-                    {
-                        AmountWithBreakdown = new AmountWithBreakdown()
+                    CancelUrl = "https://www.example.com",
+                    ReturnUrl = "https://www.example.com"
+                },
+                PurchaseUnits = new List<PurchaseUnitRequest>
+                {
+                    new PurchaseUnitRequest{
+                        AmountWithBreakdown = new AmountWithBreakdown
                         {
                             CurrencyCode = "USD",
-                            Value = "100.00"
+                            Value = "220.00"
                         }
+
                     }
-                },
-                ApplicationContext = new ApplicationContext()
-                {
-                    ReturnUrl = "https://www.example.com",
-                    CancelUrl = "https://www.example.com"
                 }
             };
 
-            response.WriteString(order.CheckoutPaymentIntent.ToString());
-
-            return response;
+            return orderRequest;
         }
     }
 }
--- PaymentMicroservices/PaymentMicroservices/Services/PaypalClientService.cs
+++ PaymentMicroservices/PaymentMicroservices/Services/PaypalClientService.cs
@@ -1,7 +1,11 @@
-﻿using PayPalCheckoutSdk.Core;
+﻿using Newtonsoft.Json;
+using PayPalCheckoutSdk.Core;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,10 +13,12 @@ namespace PaymentMicroservices.Services
 {
     public class PaypalClientService
     {
+        private PropertyInfo[] _PropertyInfos = null;
+
         static String clientId = "AVqSBkkqxw0jOZsOL1meIPnI25Ozcj12Ec8xgZqTrf6R80GftrWaxPDFvC0j4YO2K_Kyze2yNTgwhPRi";
         static String secret = "EBBGNsdoiHdTKNZk1ax86VSNmzxIYG3A1rO2m0C3A2Hoa24Z5hvrQsm2oZmnF0KF2dL3iOkQNtf7tob5";
 
-        public static PayPalHttpClient client()
+        public static PayPalHttpClient Client()
         {
             // Creating a sandbox environment
             PayPalEnvironment environment = new SandboxEnvironment(clientId, secret);
@@ -20,6 +26,23 @@ namespace PaymentMicroservices.Services
             // Creating a client for the environment
             PayPalHttpClient client = new PayPalHttpClient(environment);
             return client;
+        }
+
+
+        public override string ToString()
+        {
+            if (_PropertyInfos == null)
+                _PropertyInfos = this.GetType().GetProperties();
+
+            var sb = new StringBuilder();
+
+            foreach (var info in _PropertyInfos)
+            {
+                var value = info.GetValue(this, null) ?? "(null)";
+                sb.AppendLine(info.Name + ": " + value.ToString());
+            }
+
+            return sb.ToString();
         }
     }
 }
