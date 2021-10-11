882b944787f2efa821c8e9e74bcc1273f47f1119
Nofal <42965940+NofalB@users.noreply.github.com>
2021-10-11 18:31:23 +0200

    creating transactions from microsrvice
    added DTO for transaction,removed old ones and modified transaction http trigger

--- Domain/Transaction.cs
+++ Domain/Transaction.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Resolvers;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,92 +6,83 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
+    // Transaction myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Link
+    {
+        public string href { get; set; }
+        public string method { get; set; }
+        public string rel { get; set; }
+    }
+
+    public class Address
+    {
+        public string country_code { get; set; }
+        public string address_line_1 { get; set; }
+        public string admin_area_1 { get; set; }
+        public string admin_area_2 { get; set; }
+        public string postal_code { get; set; }
+    }
+
+    public class Name
+    {
+        public string given_name { get; set; }
+        public string surname { get; set; }
+        public string full_name { get; set; }
+    }
+
+    public class Payer
+    {
+        public Address address { get; set; }
+        public string email_address { get; set; }
+        public Name name { get; set; }
+        public string payer_id { get; set; }
+    }
+
+    public class Amount
+    {
+        public string currency_code { get; set; }
+        public string value { get; set; }
+    }
+
+    public class Payee
+    {
+        public string email_address { get; set; }
+        public string merchant_id { get; set; }
+    }
+
+    public class Shipping
+    {
+        public Address address { get; set; }
+        public Name name { get; set; }
+    }
+
+    public class PurchaseUnit
+    {
+        public Amount Amount { get; set; }
+        public Payee payee { get; set; }
+        public string reference_id { get; set; }
+        public Shipping shipping { get; set; }
+    }
+
     public class Transaction
     {
-        [OpenApiProperty(Description = "Gets or sets the a response containing a payment object. Will always contain payment for this endpoint.")]
-        [JsonRequired]
-        public string Resource { get; set; }
-
-        [OpenApiProperty(Description = "Gets or sets a unique transaction id for a transaction")]
-        [JsonRequired]
+        public DateTime create_time { get; set; }
         public string TransactionId { get; set; }
-        
-        [OpenApiProperty(Description = "Gets or sets the amount object for a transaction.")]
-        [JsonRequired]
-        public TransactionAmount Amount { get; set; }
-
-        [OpenApiProperty(Description = "Gets or sets a short description of the payment. ")]
-        [JsonRequired]
-        public string Description { get; set; }
-
-        [OpenApiProperty(Description = "Gets or sets the URL your customer will be redirected to after completing or canceling the payment process.")]
-        [JsonRequired]
-        public string RedirectUrl { get; set; }
-
-        [OpenApiProperty(Description = "Gets or sets the mode(real or test) used to create this payment")]
-        [JsonRequired]
-        public string Mode { get; set; }
-
-        [OpenApiProperty(Description = "Gets or sets the payment’s date and time of creation, in ISO 8601 format.")]
-        [JsonRequired]
-        public DateTime CreatedAt { get; set; }
-
-        [OpenApiProperty(Description = "Gets or sets the payment’s status.")]
-        [JsonRequired]
-        public string Status { get; set; }
-
-        [OpenApiProperty(Description = "Gets or sets the payment method used for this payment, either forced on creation by specifying the method parameter, or chosen by the customer on our payment method selection screen.")]
-        [JsonRequired]
-        public string Method { get; set; }
-
-        [OpenApiProperty(Description = "Gets or sets an object with several URL objects relevant to the payment. Every URL object will contain an href and a type field.")]
-        [JsonRequired]
-        public TransactionLink Link { get; set; }
-
-        public Transaction(string resource,string transactionId,TransactionAmount amount,string description,string redirectUrl,string mode,DateTime createdAt, string status,string method,TransactionLink link)
-        {
-            Resource = resource;
-            TransactionId = transactionId;
-            Amount = amount;
-            Description = description;
-            RedirectUrl = redirectUrl;
-            Mode = mode;
-            CreatedAt = createdAt;
-            Status = status;
-            Method = method;
-            Link = link;
-        }
-
-        public class DummyTransactionExample : OpenApiExample<Transaction>
-        {
-            public override IOpenApiExample<Transaction> Build(NamingStrategy NamingStrategy = null)
-            {
-                TransactionAmount transactionAmount = new TransactionAmount("EUR", "10.00");
-                TransactionLink transactionLink = new("https://api.mollie.com/v2/payments/tr_WDqYK6vllg", "https://www.mollie.com/payscreen/select-method/WDqYK6vllg");
-
-                Examples.Add(OpenApiExampleResolver.Resolve("Transaction 1", new Transaction("payment", "tr_WDqYK6vllg",transactionAmount, "Donation #12345", "https://webshop.example.org/order/12345/", "test", DateTime.Now, "open", "Paypal",transactionLink), NamingStrategy));
-                Examples.Add(OpenApiExampleResolver.Resolve("Transaction 2", new Transaction("payment", "tr_WDqYK6vllg", transactionAmount, "Donation #676", "https://webshop.example.org/order/12345/", "live", DateTime.Now, "open", "Banq", transactionLink), NamingStrategy));
-                Examples.Add(OpenApiExampleResolver.Resolve("Transaction 3", new Transaction("payment", "tr_WDqYK6vllg", transactionAmount, "Donation #234", "https://webshop.example.org/order/12345/", "test", DateTime.Now, "open", "Ideal", transactionLink), NamingStrategy));
-
-                return this;
-            }
-        }
-
-        public class DummyTransactionsExamples : OpenApiExample<List<Transaction>>
-        {
-             TransactionAmount transactionAmount = new TransactionAmount("EUR", "54.00");
-             TransactionLink transactionLink = new("https://api.mollie.com/v2/payments/tr_WDqYK6vllg", "https://www.mollie.com/payscreen/select-method/WDqYK6vllg");
-            public override IOpenApiExample<List<Transaction>> Build(NamingStrategy NamingStrategy = null)
-            {
-                Examples.Add(OpenApiExampleResolver.Resolve("Transactions", new List<Transaction> {
-                    new Transaction("payment", "tr_WDqYK6vllg",transactionAmount, "Donation #12345", "https://webshop.example.org/order/12345/", "test", DateTime.Now, "open", "Paypal",transactionLink),
-                    new Transaction("payment", "tr_WDqYK6vllg",transactionAmount, "Donation #12345", "https://webshop.example.org/order/12345/", "test", DateTime.Now, "open", "Banq",transactionLink),
-                    new Transaction("payment", "tr_WDqYK6vllg",transactionAmount, "Donation #12345", "https://webshop.example.org/order/12345/", "test", DateTime.Now, "open", "Ideal",transactionLink)
-                }));
-
-                return this;
-            }
-        }
-
+        public string intent { get; set; }
+        public List<Link> links { get; set; }
+        public Payer payer { get; set; }
+        public List<PurchaseUnit> purchase_units { get; set; }
+        public string status { get; set; }
     }
+
+    public class CreateTransaction
+    {
+        public string Message { get; set; }
+        public string Link { get; set; }
+        public string TransactionId { get; set; }
+        public string Status { get; set; }
+    }
+
+
+
 }
--- ProjectIkwambeApp/Controllers/TransactionHttpTrigger.cs
+++ ProjectIkwambeApp/Controllers/TransactionHttpTrigger.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 using Domain;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -17,55 +19,64 @@ namespace ProjectIkwambe.Controllers
     public class TransactionHttpTrigger
     {
 		ILogger Logger { get; }
+		private static HttpClient httpClient = new HttpClient();
 
 		public TransactionHttpTrigger(ILogger<TransactionHttpTrigger> Logger)
 		{
 			this.Logger = Logger;
 		}
 
-		[Function(nameof(TransactionHttpTrigger.GetTransactions))]
-		[OpenApiOperation(tags: new[] { "Transactions" }, Summary = "Get all transactions", Description = "This will retrieve all transactions", Visibility = OpenApiVisibilityType.Important)]
-		[OpenApiParameter(name: "transactionId", In = ParameterLocation.Query, Required = false, Type = typeof(string), Summary = "ID of transaction to return", Description = "Retrieves a specific transaction by ID", Visibility = OpenApiVisibilityType.Important)]
-		[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(Transaction), Summary = "Successfully fetched transactions", Description = "transactions successfully retrieved", Example = typeof(Transaction.DummyTransactionsExamples))]
-		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid transaction ID", Description = "Invalid transaction ID was provided")]
-		public async Task<HttpResponseData> GetTransactions([HttpTrigger(AuthorizationLevel.Function, "GET", Route = "transactions")] HttpRequestData req, FunctionContext executionContext)
-		{
-			{
-				HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
-				TransactionAmount transactionAmount = new TransactionAmount("EUR", "54.00");
-				TransactionLink transactionLink = new("https://api.mollie.com/v2/payments/tr_WDqYK6vllg", "https://www.mollie.com/payscreen/select-method/WDqYK6vllg");
+		//[Function(nameof(TransactionHttpTrigger.GetTransactions))]
+		//[OpenApiOperation(tags: new[] { "Transactions" }, Summary = "Get all transactions", Description = "This will retrieve all transactions", Visibility = OpenApiVisibilityType.Important)]
+		//[OpenApiParameter(name: "transactionId", In = ParameterLocation.Query, Required = false, Type = typeof(string), Summary = "ID of transaction to return", Description = "Retrieves a specific transaction by ID", Visibility = OpenApiVisibilityType.Important)]
+		//[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(Transaction), Summary = "Successfully fetched transactions", Description = "transactions successfully retrieved", Example = typeof(Transaction.DummyTransactionsExamples))]
+		//[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid transaction ID", Description = "Invalid transaction ID was provided")]
+		//public async Task<HttpResponseData> GetTransactions([HttpTrigger(AuthorizationLevel.Function, "GET", Route = "transactions")] HttpRequestData req, FunctionContext executionContext)
+		//{
+		//	{
+		//		HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
+		//		TransactionAmount transactionAmount = new TransactionAmount("EUR", "54.00");
+		//		TransactionLink transactionLink = new("https://api.mollie.com/v2/payments/tr_WDqYK6vllg", "https://www.mollie.com/payscreen/select-method/WDqYK6vllg");
 
-				List<Transaction> transactions = new List<Transaction>()
-				{
-					new Transaction("payment", "tr_WDqYK6vllg",transactionAmount, "Donation #12345", "https://webshop.example.org/order/12345/", "test", DateTime.Now, "open", "Paypal",transactionLink),
-					new Transaction("payment", "tr_WDqYK6vllg",transactionAmount, "Donation #12345", "https://webshop.example.org/order/12345/", "test", DateTime.Now, "open", "Banq",transactionLink),
-					new Transaction("payment", "tr_WDqYK6vllg",transactionAmount, "Donation #12345", "https://webshop.example.org/order/12345/", "test", DateTime.Now, "open", "Ideal",transactionLink)
-				};
+		//		List<Transaction> transactions = new List<Transaction>()
+		//		{
+		//			new Transaction("payment", "tr_WDqYK6vllg",transactionAmount, "Donation #12345", "https://webshop.example.org/order/12345/", "test", DateTime.Now, "open", "Paypal",transactionLink),
+		//			new Transaction("payment", "tr_WDqYK6vllg",transactionAmount, "Donation #12345", "https://webshop.example.org/order/12345/", "test", DateTime.Now, "open", "Banq",transactionLink),
+		//			new Transaction("payment", "tr_WDqYK6vllg",transactionAmount, "Donation #12345", "https://webshop.example.org/order/12345/", "test", DateTime.Now, "open", "Ideal",transactionLink)
+		//		};
 
-				await response.WriteAsJsonAsync(transactions);
+		//		await response.WriteAsJsonAsync(transactions);
 
-				return response;
-			}
-		}
+		//		return response;
+		//	}
+		//}
 
-		[Function(nameof(TransactionHttpTrigger.MakeTransactions))]
-		[OpenApiOperation(operationId: "transaction", tags: new[] { "Transactions" }, Summary = "Make a transaction", Description = "This will create a transaction", Visibility = OpenApiVisibilityType.Important)]
-		[OpenApiRequestBody(contentType: "application/json", bodyType: typeof(Transaction), Required = true, Description = "Transaction object for transaction details")]
-		[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(Transaction), Summary = "New transaction details", Description = "New transaction details", Example = typeof(Transaction.DummyTransactionExample))]
+		[Function(nameof(TransactionHttpTrigger.CreateTransactions))]
+		[OpenApiOperation(operationId: "transaction", tags: new[] { "Transactions" }, Summary = "Create a transaction URL", Description = "This will create a transaction link", Visibility = OpenApiVisibilityType.Important)]
+		[OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(CreateTransaction), Summary = "New transaction details", Description = "New transaction details")]
+		[OpenApiParameter(name: "currency", In = ParameterLocation.Query, Required = true, Type = typeof(string), Summary = "The currency required for the transaction creation", Description = "Creates the transaction with specified currency type", Visibility = OpenApiVisibilityType.Important)]
+		[OpenApiParameter(name: "value", In = ParameterLocation.Query, Required = true, Type = typeof(int), Summary = "The value/amount required for the transaction creation", Description = "Creates the transaction with specified value amount", Visibility = OpenApiVisibilityType.Important)]
 		[OpenApiResponseWithoutBody(statusCode: HttpStatusCode.MethodNotAllowed, Summary = "Invalid input", Description = "Invalid input")]
-		public async Task<HttpResponseData> MakeTransactions([HttpTrigger(AuthorizationLevel.Function, "POST", Route = "transactions")] HttpRequestData req, FunctionContext executionContext)
+		public async Task<HttpResponseData> CreateTransactions([HttpTrigger(AuthorizationLevel.Function, "GET", Route = "transactions")] HttpRequestData req, FunctionContext executionContext)
 		{
-			// Parse input
-			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-			Transaction transaction = JsonConvert.DeserializeObject<Transaction>(requestBody);
+			string currencyCode = HttpUtility.ParseQueryString(req.Url.Query).Get("currency");
+			string value = HttpUtility.ParseQueryString(req.Url.Query).Get("value");
+
+			var TransactionData = await httpClient.GetAsync("https://paypalmicroserviceikwambe.azurewebsites.net/api/createorder?currency="+ currencyCode + "&value="+value);
+			var TransactionDataResponseObj = await TransactionData.Content.ReadAsStringAsync();
+			var TransactionDataObj = JsonConvert.DeserializeObject<CreateTransaction>(TransactionDataResponseObj);
+
+			//// Parse input
+			//string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+			//Transaction transaction = JsonConvert.DeserializeObject<Transaction>(requestBody);
 
 			// Generate output
 			HttpResponseData response = req.CreateResponse(HttpStatusCode.OK);
 
-			await response.WriteAsJsonAsync(transaction);
+            await response.WriteAsJsonAsync(TransactionDataObj);
 
-			return response;
-		}
-	}
+            return response;
+        }
+    }
 }
 
